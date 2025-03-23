d27b96b2de96ba73d44e07a304a612ce50d683e7
이형주 <jujumom630@gmail.com>
2025-03-23 19:32:38 +0900

    스크립트로 가야되나...애니메이터로 하려면 많은거 같기도 하고

--- Assets/Scripts/Play/ChangeSprite.cs
+++ Assets/Scripts/Play/ChangeSprite.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class ChangeSprite : MonoBehaviour
 {
+    public Sprite[] Sprites_note_;
+    public Sprite[] Sprites_longnote_;
+    public Sprite[] Sprites_doublenote_;
+    public Sprite[] Sprites_multinote_;
+    public Sprite[] Sprites_note;
+    public Sprite[] Sprites_longnote;
+    public Sprite[] Sprites_doublenote;
+    public Sprite[] Sprites_multinote;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
