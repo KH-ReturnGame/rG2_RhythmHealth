48ff6f5460072223e3b769493b0a0e6779e9082b
이형주 <jujumom630@gmail.com>
2025-04-01 19:17:21 +0900

    저장 스크롤바 생성

---
+++ Assets/Scripts/UI/SaveInfo.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class SaveInfo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Save_Artist(string artistname)
+    {
+
+    }
+    public void Save_ArtistLinks(string artistname)
+    {
+
+    }
+    public void Save_Songname(string artistname)
+    {
+
+    }
+    public void Save_Author(string artistname)
+    {
+
+    }
+    public void Save_LevelDesc(string artistname)
+    {
+
+    }
+    public void Save_levelTags(string artistname)
+    {
+
+    }
+    public void Save_Difficulty(string artistname)
+    {
+
+    }
+    public void Save_BPM(string artistname)
+    {
+
+    }
+    public void Save_DamageRate(string artistname)
+    {
+
+    }
+    public void Save_speed(string artistname)
+    {
+
+    }
+}
