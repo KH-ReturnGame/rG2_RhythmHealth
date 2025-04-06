b8613c2df359c88ce2b01f12cbe30041ed46b057
이형주 <jujumom630@gmail.com>
2025-04-06 21:40:58 +0900

    BeatInfo 변수 정리

--- Assets/Scripts/RoutineMaker/BeatInfo.cs
+++ Assets/Scripts/RoutineMaker/BeatInfo.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class BeatInfo : MonoBehaviour
 {
+    string NoteType;
+    int Gym;
+    float beatsPerMinute;
+    int WaitBeat;
+    float LongTime;
+    int Multi;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
