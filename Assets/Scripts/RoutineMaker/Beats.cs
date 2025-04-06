62dd157619aa8ba9122b1f7c5ddfe007dca06eac
이형주 <jujumom630@gmail.com>
2025-04-06 16:14:43 +0900

    구조 제작 2

---
+++ Assets/Scripts/RoutineMaker/Beats.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Beats : MonoBehaviour
+{
+    public int HowLong = 0; // How long the beat lasts
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoutineMaker/MakeBeats.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MakeBeats : MonoBehaviour
+{
+    public GameObject beatPrefab; // Prefab for the beat object
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn_Beat()
+    {
+
+    }
+}
