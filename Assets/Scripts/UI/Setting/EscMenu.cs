fc223d53698805e6f3cb7433d5b7ac6852787171
이형주 <jujumom630@gmail.com>
2025-05-06 01:40:27 +0900

    ESC메뉴 활성화
    ..그니까 스크립트를 만들어놓고 적용을 안했다고?
    이새끼 뭐냐

--- Assets/Scripts/UI/Setting/EscMenu.cs
+++ Assets/Scripts/UI/Setting/EscMenu.cs
@@ -6,18 +6,6 @@ using UnityEngine.UI;
 public class EscMenu : MonoBehaviour
 {
     public GameObject _Settings;
-    GameObject _Canvas;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _Canvas = GameObject.Find("Canvas");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnEnable()
     {
